acfca26260d005c101d2f769799a4586b0994ce2
Usman Anwar <uanwar88@gmail.com>
2015-01-30 13:25:35 -0600

    Added Game and RPS classes.

--- rps.rb
+++ rps.rb
@@ -19,84 +19,99 @@ class Player
     @name = name
     @score = score
   end
-end
-
-def make_move
-  rps = ['rock','paper','scissors']
-  rps.sample
-end
-
-def play_round(player1,player2)
-  # Description: Plays a round of RPS and decides a winner. The players make a random move. The winner is given a point.
-  #
-  # Params:
-  # + player1: A player1 object.
-  # + player2: A player2 object.
-  #
-  # Returns:
-  # Returns the player object that wins, or nil if it's a tie.
   
-  rules = { 'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper' }
-  
-  player1.move = make_move
-  puts "#{player1.name} throws #{player1.move}"
-  player2.move = make_move
-  puts "#{player2.name} throws #{player2.move}"
-  
-  if rules[player1.move] == player2.move
-    puts "#{player1.name} wins the round!"
-    player1.score += 1
-    return player1
-  elsif player1.move == player2.move
-    puts "Tie!"
-    return nil
-  else
-    puts "#{player2.name} wins the round!"
-    player2.score += 1
-    return player2
+  def make_move
+    @move = ['rock','paper','scissors'].sample
   end
 end
 
-def create_players(p1,p2)
-  # Description: Creates and returns two player objects.
-  # Params:
-  # + p1: A player name string.
-  # + p2: A player name string.
+class Game
+  def self.create_players(p1,p2)
+    # Description: Creates and returns two player objects.
+    # Params:
+    # + p1: A player name string.
+    # + p2: A player name string.
   
-  return Player.new(p1), Player.new(p2)
+    return Player.new(p1), Player.new(p2)
+  end
+
+  def self.display_scores(*players)
+    # Description: Displays player scores.
+    # Params:
+    # + *players: A list of player objects.
+  
+    players.each {|x| puts "#{x.name}: #{x.score}"}
+    puts ""
+    return players
+  end
+
+  def self.display_match_winner(player1, player2)
+    # Description: Outputs the winner to the terminal.
+    # Params:
+    # + player1: player1 object.
+    # + player2: player2 object.
+  
+    puts player1.score > player2.score ? "#{player1.name} wins the match!" : "#{player2.name} wins the match!"
+  end
+
+  def self.match(game,p1,p2,best_of)
+    best_of.odd? ? win_score = (best_of/2.to_f).ceil : win_score = ((best_of-1)/2.to_f).ceil
+  
+    player1, player2 = create_players(p1,p2)
+  
+    # Have the player1 and player2 objects play a round until one of them wins
+    until player1.score == win_score || player2.score == win_score
+      game.play_round(player1,player2)
+    end  
+  
+    puts "Final scores:"
+    display_scores(player1,player2)  
+    display_match_winner(player1,player2)
+  end
 end
 
-def display_scores(*players)
-  # Description: Displays player scores.
-  # Params:
-  # + *players: A list of player objects.
+class RPS
+  def self.play_round(player1,player2)
+    # Description: Plays a round of RPS and decides a winner. The players make a random move. The winner is given a point.
+    #
+    # Params:
+    # + player1: A player1 object.
+    # + player2: A player2 object.
+    #
+    # Returns:
+    # Returns the player object that wins, or nil if it's a tie.
   
-  players.each {|x| puts "#{x.name}: #{x.score}"}
-  return players
+    player1.make_move
+    puts "#{player1.name} throws #{player1.move}"
+    player2.make_move
+    puts "#{player2.name} throws #{player2.move}"
+  
+    winner = determine_winner(player1,player2)
+  
+    puts winner ? "#{winner.name} wins the round!" : "Tie!"
+    puts ""
+  end
+
+  def self.determine_winner(player1,player2)
+    # Description: Determines the winner resulting from two player moves.
+    # Params:
+    # + player1: A player1 object.
+    # + player2: A player2 object.
+    # Returns:
+    # The player object that won the round.
+  
+    rules = { 'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper' }
+  
+    if rules[player1.move] == player2.move
+      player1.score += 1
+      return player1
+    elsif player1.move == player2.move
+      return nil
+    else
+      player2.score += 1
+      return player2
+    end
+  end
 end
 
-def display_winner(player1, player2)
-  # Description: Outputs the winner to the terminal.
-  # Params:
-  # + player1: player1 object.
-  # + player2: player2 object.
-  
-  puts player1.score > player2.score ? "#{player1.name} wins the match!" : "#{player2.name} wins the match!"
-end
-
-def match(p1,p2,best_of)
-  best_of.odd? ? win_score = (best_of/2.to_f).ceil : win_score = ((best_of-1)/2.to_f).ceil
-  
-  player1, player2 = create_players(p1,p2)
-  
-  #Have the player1 and player2 objects play a round until one of them wins
-  until player1.score == win_score || player2.score == win_score
-    play_round(player1,player2)
-  end  
-  
-  puts "Final scores:"
-  display_scores(player1,player2)  
-  display_winner(player1,player2)
-end
-
-match("Joe","Sam",5)
+Game.match(RPS,"Joe","Sam",5)
