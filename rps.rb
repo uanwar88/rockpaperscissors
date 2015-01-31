24d3d8473637669f52fe33c9575715742467f6ce
Usman Anwar <uanwar88@gmail.com>
2015-01-31 16:04:10 -0600

    Complete the file outputter.

--- rps.rb
+++ rps.rb
@@ -20,22 +20,26 @@ class Player
     @score = score
   end
   
-  def make_move(game)
-    print "List of moves: ", game.moves, "\n"
-    puts "#{name} throws: "
-    @move = gets.chomp
-    puts "#{self.name} made a move!", ""
+  def make_move(moves)
+    Outputter.put "List of moves: #{moves}\n"
+    Outputter.put "#{name} throws: "
+    @move = Moves_handler.verify(nil,moves)
+    Outputter.put "#{self.name} made a move!\n"
   end
 end
 
 class AI < Player
-  def make_move(game)
-    @move = game.moves.sample
-    puts "#{self.name} made a move!", ""
+  def make_move(moves)
+    @move = moves.sample
+    Outputter.put "#{self.name} made a move!\n"
   end
 end
 
 class Game
+  def self.file
+    @file
+  end
+  
   def self.create_players(p1, p2)
     # Description: Creates and returns two player objects.
     # Params:
@@ -57,8 +61,8 @@ class Game
     # Params:
     # + *players: A list of player objects.
   
-    players.each {|x| puts "#{x.name}: #{x.score}"}
-    puts ""
+    players.each {|x| Outputter.put "#{x.name}: #{x.score}"}
+    Outputter.put ""
     return players
   end
 
@@ -68,10 +72,14 @@ class Game
     # + player1: player1 object.
     # + player2: player2 object.
   
-    puts player1.score > player2.score ? "#{player1.name} wins the match!" : "#{player2.name} wins the match!"
+    player1.score > player2.score ? Outputter.put("#{player1.name} wins the match!") : Outputter.put("#{player2.name} wins the match!")
   end
 
-  def self.match(game,p1,p2,best_of)
+  def self.match(game,file=0,p1,p2,best_of)
+    if file == 1
+      @file = File.open("game_output.txt", "w")
+    end
+    
     best_of.odd? ? win_score = (best_of / 2) + 1 : win_score = (best_of / 2) + 1
     
     if p1 == p2
@@ -84,20 +92,24 @@ class Game
     # Have the player1 and player2 objects play a round until one of them wins
     round = 1
     until player1.score == win_score || player2.score == win_score
-      puts "Round #{round}!"
+      Outputter.put("Round #{round}!")
       game.play_round(player1,player2)
       round += 1
     end  
   
-    puts "Final scores:"
+    Outputter.put "Final scores:"
     display_scores(player1,player2)  
     display_match_winner(player1,player2)
+    
+    if Game.file
+      @file.close
+    end
   end
 end
 
 class RPS
   def self.moves  
-    @moves = ['rock','paper','scissors']
+    ['rock','paper','scissors']
   end
   
   def self.play_round(player1,player2)
@@ -110,14 +122,14 @@ class RPS
     # Returns:
     # Returns the player object that wins, or nil if it's a tie.
   
-    player1.make_move(self)    
-    player2.make_move(self)
+    player1.make_move(self.moves)    
+    player2.make_move(self.moves)
   
     winner = determine_winner(player1,player2)
     
-    puts "#{player1.name} threw #{player1.move}. #{player2.name} threw #{player2.move}."
-    puts winner ? "#{winner.name} wins the round!" : "Tie!"
-    puts ""
+    Outputter.put "#{player1.name} threw #{player1.move}. #{player2.name} threw #{player2.move}."
+    winner ? Outputter.put("#{winner.name} wins the round!") : Outputter.put("Tie!")
+    Outputter.put ""
   end
 
   def self.determine_winner(player1,player2)
@@ -142,4 +154,75 @@ class RPS
   end
 end
 
-Game.match(RPS,"AI_Joe","AI_Sam",3)
+class RPSLS
+  def self.moves  
+    ['rock','paper','scissors','lizard','spock']
+  end
+  
+  def self.play_round(player1,player2)
+    # Description: Plays a round of RPS and decides a winner. The players make a random move. The winner is given a point.
+    #
+    # Params:
+    # + player1: A player1 object.
+    # + player2: A player2 object.
+    #
+    # Returns:
+    # Returns the player object that wins, or nil if it's a tie.
+  
+    player1.make_move(self.moves)    
+    player2.make_move(self.moves)
+  
+    winner = determine_winner(player1,player2)
+    
+    Outputter.put "#{player1.name} threw #{player1.move}. #{player2.name} threw #{player2.move}."
+    Outputter.put winner ? "#{winner.name} wins the round!" : "Tie!"
+    Outputter.put ""
+  end
+
+  def self.determine_winner(player1,player2)
+    # Description: Determines the winner resulting from two player moves.
+    # Params:
+    # + player1: A player1 object.
+    # + player2: A player2 object.
+    # Returns:
+    # The player object that won the round.
+  
+    rules = { 'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper', 'lizard' => 'paper', 'spock' => 'rock' }
+    rules2 = { 'rock' => 'lizard',  'paper' => 'spock', 'scissors' => 'lizard', 'lizard' => 'spock', 'spock' => 'scissors' }
+  
+    if rules[player1.move] == player2.move || rules2[player1.move] == player2.move
+      player1.score += 1
+      return player1
+    elsif player1.move == player2.move
+      return nil
+    else
+      player2.score += 1
+      return player2
+    end
+  end
+end
+
+class Moves_handler
+  def self.verify(move,moves)
+    loop do
+      print "Select a valid move from the list: #{moves}\n"
+      move = gets.chomp
+      break if moves.index(move)
+    end
+    return move
+  end
+end
+
+class Outputter
+  def self.put(string)
+    if Game.file
+      Game.file.puts string
+      puts string
+    else
+      puts string
+    end
+  end
+end
+  
+
+Game.match(RPSLS,"AI_Usman","AI_Sam",3)
